329efeb0110cd3e94ad8f3d304d4e3e77cfdd334
Sluchevska <alaska3a@gmail.com>
2021-07-01 15:26:50 +0100

    update sass

--- src/colorswitch.js
+++ src/colorswitch.js
@@ -1,3 +1,5 @@
+import './sass/main.scss'
+
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -11,18 +13,25 @@ const startBtn = document.querySelector("[data-action='start']");
 const stopBtn = document.querySelector("[data-action='stop']");
 
 startBtn.addEventListener('click', randomColorSwitch);
+stopBtn.addEventListener('click', clearInterval);
 
 function randomColorSwitch() {
+    
+    
+    // bodyRef.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+  let intervalID = setInterval(function () {
     const randomIntegerFromInterval = (min, max) => {
         return Math.floor(Math.random() * (max - min + 1) + min);
          
     };
     
     bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0,6)]
-    
-    // bodyRef.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-  
+  },1000)
     
     
+};
+
+function clearInterval() {
+  clearInterval(intervalID)
 };
 
